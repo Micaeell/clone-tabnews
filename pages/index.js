0dfbd3b648d7ad7f70f82d87fb378e1e853e97ac
Micael Silva <32531283+Micaeell@users.noreply.github.com>
2024-07-31 23:40:37 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Já to profissional nisso aqui, mereço até um beijo!</h1>
+    return <h1>Já to profissional nisso aqui, mereço até um beijo 😘!</h1>
 }
 
 export default Home;
